d68fae8724eace124b98bc0ebf922e1f57bcfc2d
ilkay <ilkay997@gmail.com>
2022-10-13 19:31:53 +0100

    stupid

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,20 @@
 class QuestionsController < ApplicationController
 
   def ask
+
   end
 
+  def answer
+    @user = params[:user]
 
+    if @user == "I am going to work now"
+        @answer = "Amazing"
+    elsif @user.ends_with?("?")
+        @answer = "Silly,question, get dressed and go to work!."
+    else
+        @answer = "I don't care, get dressed and go to work!"
+
+
+    end
+  end
 end
